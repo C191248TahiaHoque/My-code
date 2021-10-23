c18595f61c9bda5654d65cf6b93d42c641557b68
Tahia Hoque <89339274+C191248TahiaHoque@users.noreply.github.com>
2021-10-24 01:05:04 +0600

    Add files via upload

---
+++ save file.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+//echo '<pre>';
+//print_r($_GET);
+
+//var_dump();
+getData();//Function is being called
+
+function getData()
+{
+    $studentName = $_POST['studentName'];
+    $Gender = $_POST['Gender'];
+    $Department = $_POST['Department'];
+    $Roll = $_POST ['Roll'];
+    //$gender=$_POST['gender'];
+
+    include "database_connection.php";
+
+
+    $insert_query = "INSERT INTO students(studentName,Gender,Department,Roll) values('$studentName','$Gender','$Department','$Roll')";
+    $insert_query = mysqli_query($connection, $insert_query);
+
+    if ($insert_query) {
+        echo 'Data has been saved successfully';
+    } else {
+        echo 'Data has not been saved successfully';
+  
+  }
+  header("Location:registration page.php");
+  
+}
+
+?>
+    
---
+++ signin.php
@@ -0,0 +1,29 @@
+<?php
+include 'header.php';
+?>
+<form method="post" action="store.php">
+    <table>
+        <tr>
+            <td>Name</td>
+            <td>
+                <input name="studentName" placeholder="Enter student name" id="studentName">
+            </td>
+        </tr>
+        <tr>
+            <td>Roll</td>
+            <td>
+                <input name="studentId" placeholder="Enter roll" id="studentId">
+            </td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>
+                <input type="submit" name="submitButton" value="Save">
+            </td>
+        </tr>
+    </table>
+</form>
+
+<?php
+include 'footer.php';
+?>
